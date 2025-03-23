9443ab461503d7f65a8125feb1c6308509c6e0c3
Tyler Moessing <tmoessing@gmail.com>
2025-03-23 02:01:24 -0600

    attempted port fix

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -20,7 +20,7 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(8080);
+        var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
         facade = new ServerFacade("http://localhost:" + port);
     }
