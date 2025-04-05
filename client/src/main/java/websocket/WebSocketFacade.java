4d9f985572fe21e77cf6498af0c1edd3da53181c
Tyler Moessing <tmoessing@gmail.com>
2025-04-04 23:38:29 -0600

    add endpoint

--- client/src/main/java/websocket/WebSocketFacade.java
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -1,4 +1,12 @@
 package websocket;
 
-public class WebSocketFacade {
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.Session;
+
+public class WebSocketFacade extends Endpoint {
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+        //Endpoint requires this method, but you don't have to do anything
+    }
 }
