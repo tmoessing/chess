7dd155e7233794804c8b1de83f35ac15168c0d5e
Tyler Moessing <tmoessing@gmail.com>
2025-04-04 23:30:51 -0600

    moved WebSocket to Server

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,11 +1,15 @@
 package server.websocket;
 
-import org.eclipse.jetty.websocket.api.annotations.WebSocket;
-
+import org.eclipse.jetty.websocket.api.*;
+import org.eclipse.jetty.websocket.api.annotations.*;
 
 @WebSocket
 public class WebSocketHandler {
 
     private final ConnectionManager connections = new ConnectionManager();
 
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws Exception {
+        session.getRemote().sendString("WebSocket response: " + message);
+    }
 }
